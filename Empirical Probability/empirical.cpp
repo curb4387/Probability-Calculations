d0e36f7255dfc4eb18e02cacb2ee9b3dca95ef26
curb4387 <teenawtran@gmail.com>
2025-09-29 15:04:18 -0400

    Update main for function testing

--- Empirical Probability/empirical.cpp
+++ Empirical Probability/empirical.cpp
@@ -66,14 +66,15 @@ int main()
               << "1) Enter the number of times the favorable event occured,\n"
               << "2) Enter the number of times the experiment was performed.\n";
 
-    std::cout << "Number of times the favorable event occurred\n";
+    std::cout << "\nNumber of times the favorable event occurred\n";
     int event = input_number();
 
     std::cout << "Number of times the experiment was performed\n";
     int experiment = input_number();
-    
+
     double prob = calculate_empirical(event, experiment);
-    std::cout << prob << "\n";
+    std::cout << "\nThe empirical probability is " << event << " / "
+              << experiment << " = " << prob << "%\n";
 
     return 0;
 }
